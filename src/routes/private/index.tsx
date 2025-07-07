50d427399f0aada61e5a7daa578554ad4329ee2d
Shafayat <shafayat1777@gmail.com>
2025-07-07 12:49:17 +0600

    ci: :sparkles: Added library routes in Route/index page

--- src/routes/private/index.tsx
+++ src/routes/private/index.tsx
@@ -2,9 +2,10 @@ import { IRoute } from '@/types';
 import { cloneDeep } from 'lodash';
 
 import HrRoutes from './hr';
+import LibraryRoutes from './libirary';
 import ProfileRoutes from './profile';
 
-const privateRoutes: IRoute[] = [...HrRoutes, ...ProfileRoutes];
+const privateRoutes: IRoute[] = [...HrRoutes, ...ProfileRoutes, ...LibraryRoutes];
 
 const privateRoutesClone = cloneDeep(privateRoutes);
 
